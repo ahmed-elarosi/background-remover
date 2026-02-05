7dfebbfc2681405154ff40d312f14aa5166d8803
Ahmed Elarosi <cabry95@protonmail.com>
2026-02-05 08:14:02 +0100

    get all the elements by id

---
+++ app.js
@@ -0,0 +1,4 @@
+const imageInput = document.getElementById("imageInput");
+const removeBtn = document.getElementById("removeBtn");
+const resultImage = document.getElementById("resultImage");
+const downloadLink = document.getElementById("downloadLink");
